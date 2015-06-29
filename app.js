45f58166c6d38e695e055a892376c44a763d8de2
River Ginther <river.ginther@banno.com>
2015-06-29 16:39:06 -0500

    Added information transfer to the Profile page through a "View" button from the User list in order to view a profile.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ sampleApp.config(['$routeProvider',
                 templateUrl: 'templates/ListUsers.html',
                 controller: 'ListUsersController'
             }).
-            when('/users/:Id', {
+            when('/users/:id', {
                 templateUrl: 'templates/UserProfile.html',
                 controller: 'UserProfileController'
             }).
@@ -40,6 +40,7 @@ sampleApp.controller('ListUsersController', ['$scope', 'UsersService',
     }
 
     $scope.add = function() {
+      alert('User is created!');
       UsersService.add({}, {
         'firstName': this.firstName,
         'lastName': this.lastName,
@@ -54,29 +55,40 @@ sampleApp.controller('ListUsersController', ['$scope', 'UsersService',
 sampleApp.controller('UserProfileController', ['$scope', '$routeParams', 'UsersService',
   function($scope, $routeParams, UsersService) {
 
+      //Looping through the users for the id
+      UsersService.query({}, function(users) {
+        for(var i = 0; i < users.length; i++) {
+          if (users[i]['_id'] == $routeParams.id) {
+            $scope.user = users[i];
+            break;
+          }
+        }
+      });
+      
+
     UsersService.query({}, function(users) {
       $scope.users = users;
-      
+
       //Start of editing
       $scope.editorEnabled = false;
     
       $scope.enableEditor = function() {
         $scope.editorEnabled = true;
-        $scope.editablefirstName = $scope.users.firstName;
-        $scope.editablelastName = $scope.users.lastName;
-        $scope.editablephone = $scope.users.phone;
-        $scope.editableemail = $scope.users.email;
+        $scope.editablefirstName = $scope.user.firstName;
+        $scope.editablelastName = $scope.user.lastName;
+        $scope.editablephone = $scope.user.phone;
+        $scope.editableemail = $scope.user.email;
       };
       
       $scope.disableEditor = function() {
         $scope.editorEnabled = false;
       };
       
-      $scope.save = function() {
-        $scope.users.firstName = $scope.editablefirstName;
-        $scope.users.lastName = $scope.editablelastName;
-        $scope.users.phone = $scope.editablephone;
-        $scope.users.email = $scope.editableemail;
+      $scope.update = function() {
+        UsersService.user.firstName = $scope.editablefirstName;
+        $scope.user.lastName = $scope.editablelastName;
+        $scope.user.phone = $scope.editablephone;
+        $scope.user.email = $scope.editableemail;
         $scope.disableEditor();
       };
     });
--- services.js
+++ services.js
@@ -6,7 +6,7 @@ var sampleAppservices = angular.module('sampleAppservices', ['ngResource']);
 sampleAppservices.factory('UsersService', ['$resource',
   function($resource){
     return $resource('http://localhost:24149/users/:id', {}, {
-      'query': {method:'GET', isArray:true},
+      'query': {method:'GET', params: { id: '@id'}, isArray:true},
       'add': {method: 'POST', headers: { 'Content-Type': 'application/json'}},
       'update': {method: 'PUT', headers: { 'Content-Type': 'application/json'}},
       'remove': {method: 'DELETE'},
